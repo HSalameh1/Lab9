3f6e9ea90e73228c0c864982329c4023dcf8036d
Helwa <helwasalameh14@gmail.com>
2025-05-12 21:52:41 -0500

    "Split the Switches interface into smaller, more focused interfaces"

---
+++ src/main/java/org/example/Solid/InterfaceSeggregation/Example1/Solution/RadioSwitch.java
@@ -0,0 +1,12 @@
+//------------------------------------------------------
+// Lab 9
+// Written by: Helwa Salameh [1961163]
+// For SE 350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
+package org.example.Solid.InterfaceSeggregation.Example1.Solution;
+
+public interface RadioSwitch {
+    void turnRadioOn();
+    void turnRadioOff();
+}
