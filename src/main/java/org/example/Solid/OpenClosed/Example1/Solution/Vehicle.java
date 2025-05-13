4421ca995b34c3bd0bb8a566958ea9dc41242ca1
Helwa <helwasalameh14@gmail.com>
2025-05-12 21:13:39 -0500

    "Created Vehicle file for the soultion folder"

---
+++ src/main/java/org/example/Solid/OpenClosed/Example1/Solution/Vehicle.java
@@ -0,0 +1,28 @@
+//------------------------------------------------------
+// Lab 9
+// Written by: Helwa Salameh [1961163]
+// For SE 350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
+package org.example.Solid.OpenClosed.Example1.Solution;
+
+public class Vehicle {
+    private int power;
+    private int suspensionHeight;
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(final int power) {
+        this.power = power;
+    }
+
+    public int getSuspensionHeight() {
+        return suspensionHeight;
+    }
+
+    public void setSuspensionHeight(final int suspensionHeight) {
+        this.suspensionHeight = suspensionHeight;
+    }
+}
