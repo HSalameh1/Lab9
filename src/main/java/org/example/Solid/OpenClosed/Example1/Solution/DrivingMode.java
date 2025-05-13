5aa9ebbb14033a5dbb6db528bfae1c1076ba6287
Helwa <helwasalameh14@gmail.com>
2025-05-12 21:12:25 -0500

    "Created DrivingMode file for the soultion folder"

---
+++ src/main/java/org/example/Solid/OpenClosed/Example1/Solution/DrivingMode.java
@@ -0,0 +1,13 @@
+//------------------------------------------------------
+// Lab 9
+// Written by: Helwa Salameh [1961163]
+// For SE 350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
+package org.example.Solid.OpenClosed.Example1.Solution;
+
+public interface DrivingMode {
+    int getPower();
+    int getSuspensionHeight();
+}
+
